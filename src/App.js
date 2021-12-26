539ff637db0b65df2e6534a474e7ec9c80482a45
RikiHildesaimmer <riki4100475@gmail.com>
2021-12-26 16:16:45 +0200

    newComponentPrize

--- src/App.js
+++ src/App.js
@@ -1,16 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
 
+import Prize from './Prize';
+
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload. Deploy-work!
-        </p>
-      </header>
-    </div>
+    <>
+
+    </>
   );
 }
 
