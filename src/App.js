508e640e4a5c9bed62e2cab61a6d98971449fa93
ZipiWerner <zipi100100@gmail.com>
2021-12-26 15:54:00 +0200

    startAppBar

--- src/App.js
+++ src/App.js
@@ -1,16 +1,19 @@
 import logo from './logo.svg';
 import './App.css';
+import ButtonAppBar from './AppBar';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
+      {/* <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload. Deploy-work!
         </p>
-      </header>
+      </header> */}
+      <ButtonAppBar/>
     </div>
+   
   );
 }
 
