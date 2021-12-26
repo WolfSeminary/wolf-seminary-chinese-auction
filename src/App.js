f2f6d36075c83c5dab846d591028c759c6e5413a
malka-308 <mat33308@gmail.com>
2021-12-26 16:54:49 +0200

    firstPayButtonComponent

--- src/App.js
+++ src/App.js
@@ -1,15 +1,13 @@
-import logo from './logo.svg';
 import './App.css';
+import PayButton  from './Button';
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload. Deploy-work!
-        </p>
-      </header>
+      {/* <header className="App-header"> */}
+       <PayButton className="payButton"/>
+      {/* </header> */}
     </div>
   );
 }
