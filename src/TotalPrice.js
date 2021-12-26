8558015866549bc80a07f8a10ee5b1b7e2fafa47
efratamar <efratamargit@gmail.com>
2021-12-26 16:50:25 +0200

    BtnTotalPrice

---
+++ src/TotalPrice.js
@@ -0,0 +1,8 @@
+import React from 'react'
+import { TextField } from '@mui/material';
+ const TotalPrice=()=>{
+return(
+    <TextField  id="outlined-basic" label="סך לתשלום" variant="outlined" disabled={true} value="120"/>
+)
+}
+export default TotalPrice;
