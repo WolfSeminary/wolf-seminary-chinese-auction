cc6fb4f84cf8588f61da782ad7ad199857857691
sokolovski-sara <s0534187671@gmail.com>
2021-12-28 15:56:06 +0200

    add a priceTable component

--- src/PricesTable.js
+++ src/PricesTable.js
@@ -1,66 +1,21 @@
-import * as React from 'react';
-import Table from '@mui/material/Table';
-import TableBody from '@mui/material/TableBody';
-import TableCell from '@mui/material/TableCell';
-import TableContainer from '@mui/material/TableContainer';
-import TableHead from '@mui/material/TableHead';
-import TableRow from '@mui/material/TableRow';
-import Paper from '@mui/material/Paper';
+import { TableContainer,TableCell,TableBody,TableHead,Table } from '@mui/material';
 
-
-
-export default function BasicTable() {
-  return (
-    <TableContainer component={Paper}>
-      <Table sx={{ minWidth: 650 }} aria-label="simple table">
-        <TableHead>
-          <TableRow>
-            <TableCell>Dessert (100g serving)</TableCell>
-            <TableCell align="right">Calories</TableCell>
-            <TableCell align="right">Fat&nbsp;(g)</TableCell>
-            <TableCell align="right">Carbs&nbsp;(g)</TableCell>
-            <TableCell align="right">Protein&nbsp;(g)</TableCell>
-          </TableRow>
-        </TableHead>
-        <TableBody>
-          
-            <TableRow
-              key={row.name}
-              sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
-            >
-              <TableCell component="th" scope="row">
-                {row.name}
-              </TableCell>
-              <TableCell align="right">{row.calories}</TableCell>
-              <TableCell align="right">{row.fat}</TableCell>
-              <TableCell align="right">{row.carbs}</TableCell>
-              <TableCell align="right">{row.protein}</TableCell>
-            </TableRow>
-          )
-        </TableBody>
-      </Table>
-    </TableContainer>
-  );
+const PricesTable = () => {
+    return (
+        <TableContainer>
+            <Table >
+                <TableHead >
+                    <TableCell align="center">מחיר</TableCell>
+                    <TableCell align="center">כמות</TableCell>
+                    <TableCell align="center">פריט</TableCell>
+                </TableHead>
+                <TableBody>
+                    <TableCell align="center">2000</TableCell>
+                    <TableCell align="center">2</TableCell>
+                    <TableCell align="center">חדר ילדים </TableCell>
+                </TableBody>
+            </Table>
+        </TableContainer>
+        )
 }
-
-// import { TableContainer,TableCell,TableBody,TableHead,Table } from '@mui/material';
-
-// const PricesTable = () => {
-//     return (
-//         <TableContainer>
-//             <Table>
-//                 <TableHead >
-//                     <TableCell align="center">פריט</TableCell>
-//                     <TableCell align="center">כמות</TableCell>
-//                     <TableCell align="center">מחיר</TableCell>
-//                 </TableHead>
-//                 <TableBody>
-//                     <TableCell align="center"></TableCell>
-//                     <TableCell align="center"></TableCell>
-//                     <TableCell align="center"></TableCell>
-//                 </TableBody>
-//             </Table>
-//         </TableContainer>
-//         )
-// }
-// export default PricesTable
+export default PricesTable
--- src/index.js
+++ src/index.js
@@ -3,11 +3,10 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import PricesTable from './PricesTable';
 
 ReactDOM.render(
   <React.StrictMode>
-   <PricesTable/>
+    <App/>
   </React.StrictMode>,
   document.getElementById('root')
 );
