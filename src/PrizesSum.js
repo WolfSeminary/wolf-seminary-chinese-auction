6dd0d2e9cdd2418ca062f9c5c993fed7349b4bbf
sokolovski-sara <s0534187671@gmail.com>
2022-01-02 18:53:40 +0200

    Sum Prizes - Manage State

---
+++ src/PrizesSum.js
@@ -0,0 +1,9 @@
+import { useState } from "react"
+
+const PrizesSum = () => {
+    const [numOfSelectedPrizes, setNumOfSelectedPrizes] = useState(0);
+    const onSelectedPrizeChange = () => {
+
+    }
+}
+export default PrizesSum
