763191604703e9bd736a47b80e9511fa640666c0
malka-308 <mat33308@gmail.com>
2022-01-02 18:21:22 +0200

    payButtomWithoutApp

--- src/App.js
+++ src/App.js
@@ -1,13 +1,9 @@
 import './App.css';
-import PayButton  from './Button';
 
 
 function App() {
   return (
     <div className="App">
-      {/* <header className="App-header"> */}
-       <PayButton className="payButton"/>
-      {/* </header> */}
     </div>
   );
 }
