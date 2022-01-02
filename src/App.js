9fdb334971dc0231538ddb2f83dd62cc30503b4a
sariYagelnik <96356437+sariYagelnik@users.noreply.github.com>
2022-01-02 17:58:22 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
 import logo from './logo.svg';
 import './App.css';
-import SubmitButton from './SubmitButton';
 
 function App() {
   return (
-    <SubmitButton/>
+     <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload. Deploy-work!
+        </p>
+      </header>
+    </div>
   );
 }
 
