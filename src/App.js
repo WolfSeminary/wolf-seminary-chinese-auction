c09206f5297c0fc2f7831cf5ff0176206298f1c3
eini-rc <REini@madisoncres.com>
2022-01-09 23:06:23 +0200

    test(Deploy): remove styled-components

--- src/App.js
+++ src/App.js
@@ -1,14 +1,15 @@
+import logo from './logo.svg';
 import './App.css';
-import TotalPrice from './TotalPrice';
-import React from 'react';
-import BonussCard from './NoticeModal';
-
-
 
 function App() {
   return (
     <div className="App">
-      <BonussCard />
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload. Deploy-work!
+        </p>
+      </header>
     </div>
   );
 }
