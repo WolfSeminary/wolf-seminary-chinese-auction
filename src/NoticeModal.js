6bd71aea5228eb9aaf66291183a44d7d71621236
racheli-cohen <96010447+racheli-cohen@users.noreply.github.com>
2022-01-09 13:13:47 +0200

    feat(NoticeModal): component Init State (#40)
    * format document

--- src/NoticeModal.js
+++ src/NoticeModal.js
@@ -1,61 +1,64 @@
-import * as React from 'react';
+import React, { useState } from 'react';
 import Modal from '@mui/material/Modal';
 import Typography from '@mui/material/Typography';
 import Box from '@mui/material/Box';
 
-const BonussCard=()=> {
+const BonussCard = () => {
   const rootRef = React.useRef(null);
+  const [shouldShowNoticeModal, SetshouldShowNoticeModal] = useState(false)
 
   return (
-    <Box
-      sx={{
-        height: 300,
-        flexGrow: 1,
-        minWidth: 300,
-        transform: 'translateZ(0)',
-        // The position fixed scoping doesn't work in IE11.
-        // Disable this demo to preserve the others.
-        '@media all and (-ms-high-contrast: none)': {
-          display: 'none',
-        },
-      }}
-      ref={rootRef}
-    >
-      <Modal
-        disablePortal
-        disableEnforceFocus
-        disableAutoFocus
-        open
-        aria-labelledby="server-modal-title"
-        aria-describedby="server-modal-description"
-        sx={{
-          display: 'flex',
-          p: 1,
-          alignItems: 'center',
-          justifyContent: 'center',
-        }}
-        container={() => rootRef.current}
-      >
+    <>
+      {shouldShowNoticeModal &&
         <Box
           sx={{
-            position: 'relative',
-            width: 400,
-            bgcolor: 'background.paper',
-            border: '2px solid #000',
-            boxShadow: (theme) => theme.shadows[5],
-            p: 4,
+            height: 300,
+            flexGrow: 1,
+            minWidth: 300,
+            transform: 'translateZ(0)',
+            // The position fixed scoping doesn't work in IE11.
+            // Disable this demo to preserve the others.
+            '@media all and (-ms-high-contrast: none)': {
+              display: 'none',
+            },
           }}
+          ref={rootRef}
         >
-          <Typography id="server-modal-title" variant="h6" component="h2">
-            !כרטיס בונוס
-          </Typography>
-          <Typography id="server-modal-description" sx={{ pt: 2 }}>
-         
-           קנית 3 כרטיסים מאותה קטגוריה בחר מתנה נוספת בחינם 
-          </Typography>
-        </Box>
-      </Modal>
-    </Box>
+          <Modal
+            disablePortal
+            disableEnforceFocus
+            disableAutoFocus
+            open
+            aria-labelledby="server-modal-title"
+            aria-describedby="server-modal-description"
+            sx={{
+              display: 'flex',
+              p: 1,
+              alignItems: 'center',
+              justifyContent: 'center',
+            }}
+            container={() => rootRef.current}
+          >
+            <Box
+              sx={{
+                position: 'relative',
+                width: 400,
+                bgcolor: 'background.paper',
+                border: '2px solid #000',
+                boxShadow: (theme) => theme.shadows[5],
+                p: 4,
+              }}
+            >
+              <Typography id="server-modal-title" variant="h6" component="h2">
+                !כרטיס בונוס
+              </Typography>
+              <Typography id="server-modal-description" sx={{ pt: 2 }}>
+                קנית 3 כרטיסים מאותה קטגוריה בחר מתנה נוספת בחינם
+              </Typography>
+            </Box>
+          </Modal>
+        </Box>}
+    </>
   );
 }
 export default BonussCard;
