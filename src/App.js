c8069185e7ff29393aec78ed346bf621a22120f1
ZipiWerner <zipi100100@gmail.com>
2022-01-10 14:33:05 +0200

    a

--- src/App.js
+++ src/App.js
@@ -4,12 +4,7 @@ import Prizes from './Prizes.js'
 function App() {
   return (
     <div className="App">
-      {/* <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload. Deploy-work!
-        </p>
-      </header> */}
+
      < Prizes/>
     </div>
   );
