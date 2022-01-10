502e96e402ce98d85df2476661714c9e29e04345
Zipi Werner <96010136+ZipiWerner@users.noreply.github.com>
2022-01-10 16:06:03 +0200

    feat(HomePage) : Add an App bar (#11)

---
+++ src/AppBarPage.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import AppBar from '@mui/material/AppBar';
+import Box from '@mui/material/Box';
+import Toolbar from '@mui/material/Toolbar';
+import Typography from '@mui/material/Typography';
+export default function AppBarPage() {
+    return (
+        <Box sx={{ flexGrow: 1 }}>
+            <AppBar position="static">
+                <Toolbar>
+                    <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+                        ברוכים הבאים למכירת החסד של סמינר וולף
+                        <p style={{ margin: '0px' }}>מפעל הבית</p>
+                    </Typography>
+                </Toolbar>
+            </AppBar>
+        </Box>
+    );
+}
