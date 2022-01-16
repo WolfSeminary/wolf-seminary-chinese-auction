229314b074229f9a95de56fb30be870057708280
Sara-Havkin-10 <s0556776115@gmail.com>
2022-01-16 15:22:36 +0200

    update master

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
-
+import React from 'react';
+import logo from './logo.svg';
 import './App.css';
-import BackButton from './BackButton';
 
 function App() {
   return (
     <div className="App">
-      <BackButton/>
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload. Deploy-work!
+        </p>
+      </header>
     </div>
   );
 }
