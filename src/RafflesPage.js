e8dcc5050c8e46d3e91811d38959ec4881e31198
Malki Turchin <96010359+malka-308@users.noreply.github.com>
2022-01-16 15:05:31 +0200

    feat(Raffles): Add Raffles Page (#64)

---
+++ src/RafflesPage.js
@@ -0,0 +1,12 @@
+import React from "react";
+import AppBarPage from "./AppBarPage";
+
+const RafflesPage = () => {
+    return (<>
+        <AppBarPage></AppBarPage>
+        {/* <RafflesTable></RafflesTable> */}
+        {/* <Winner></Winner> */}
+    </>)
+}
+
+export default RafflesPage;
