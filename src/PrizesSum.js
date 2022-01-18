245c69675bf82c60e6f496f6210c75d54021f697
malki-308 <mat33308@gmail.com>
2022-01-18 18:21:10 +0200

    merge

---
+++ src/PrizesSum.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import Badge from '@mui/material/Badge';
+import MailIcon from '@mui/icons-material/Mail';
+
+const PrizesSum = () => {
+    return (
+        <Badge badgeContent={0} color="primary">
+            <MailIcon color="action" />
+        </Badge>
+    )
+}
+
+export default PrizesSum
