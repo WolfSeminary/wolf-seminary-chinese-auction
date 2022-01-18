a4d21f4494a9ae37885a5cf11bbdb72e898cc2d9
sariYagelnik <96356437+sariYagelnik@users.noreply.github.com>
2022-01-18 19:23:48 +0200

    add sort abillity (#77)

--- src/PricesTable.js
+++ src/PricesTable.js
@@ -1,20 +1,61 @@
-import { TableContainer, TableCell, TableBody, TableHead, Table,TableRow } from '@mui/material';
+
+
+import { TableContainer, TableCell, TableBody, TableHead, Table, TableRow, Button } from '@mui/material';
+import ArrowDropDownIcon from '@mui/icons-material/ArrowDropDown';
+import { useState } from 'react';
+
+const PricesTable = (props) => {
+    const [prizes, setPrizes] = useState(
+        [
+            {
+                id: 1,
+                name: 'bbb',
+                price: 20,
+                count: 3
+            },
+            {
+                id: 2,
+                name: 'ccc',
+                price: 20,
+                count: 3
+            },
+            {
+                id: 3,
+                name: 'aaa',
+                price: 20,
+                count: 3
+            }
+        ]
+    )
+    const sort = () => {
+        const all=[...prizes]
+        setPrizes(all.sort((s1, s2) => s1.name.localeCompare(s2.name)))
+    }
+
 
 const PricesTable = (props) => {
     return (
         <TableContainer>
             <Table>
                 <TableHead>
-                    <TableCell align="center">מחיר</TableCell>
-                    <TableCell align="center">כמות</TableCell>
-                    <TableCell align="center">פריט</TableCell>
+                    <TableRow>
+                        <TableCell align="center">מחיר</TableCell>
+                        <TableCell align="center">כמות</TableCell>
+                        <TableCell align="center">
+                            <Button onClick={sort} variant="outlined" startIcon={<ArrowDropDownIcon />}>
+                                (מיין) פריט
+                            </Button>
+                        </TableCell>
+                    </TableRow>
                 </TableHead>
                 <TableBody>
+
                     {props.prizes.map(item =>
                         <TableRow key={item.id}>
                             <TableCell align="center">{item.price}</TableCell>
                             <TableCell align="center">{item.count}</TableCell>
                             <TableCell align="center">{item.id}</TableCell>
+
                         </TableRow>)}
                 </TableBody>
             </Table>
