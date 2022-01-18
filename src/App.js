15e6b722b5a05a8bc28b3f291726357ce5d82957
malki-308 <mat33308@gmail.com>
2022-01-18 17:43:17 +0200

    ad

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
-
+import React from 'react';
+import { Route, Routes } from 'react-router-dom';
 import './App.css';
-import BackButton from './button1';
+import BackButton from './BackButton';
+import PaymentPage from './PaymentPage';
+import Prizes from './Prizes';
 
 function App() {
   return (
     <div className="App">
-      <BackButton/>
+      <Routes>
+        <Route path="Prizes" element={<Prizes />} />
+        <Route path="PaymentPage" element={<PaymentPage />} />
+      </Routes>
     </div>
   );
 }
