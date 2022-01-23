6ba3b17e0a791954a011d759e0d708237e3631ba
rachel-2863 <rachelz2863@gmail.com>
2022-01-23 15:52:55 +0200

    feat(TotalPrize): changes in totalprize

--- src/PaymentPage.js
+++ src/PaymentPage.js
@@ -6,39 +6,12 @@ import React, { useState } from "react";
 import AppBAr from "./AppBar";
 import BackButton from './BackButton'
 
-const PaymentPage = () => {
+const PaymentPage = (props) => {
     const [shouldShowThankYouModal, setShouldShowThankYouModal] = useState(false);
-    let prizes = [
-        {
-            id: 1,
-            price: 10,
-            item: "cumputer",
-        },
-        {
-            id: 2,
-            price: 20,
-            item: "bed"
-        },
-        {
-            id: 3,
-            price: 200,
-            item: "kandy"
-        },
-        {
-            id: 4,
-            price: 200,
-            item: "kandy"
-        },
-        {
-            id: 5,
-            price: 200,
-            item: "andy"
-        },
-    ]
     return (<>
         <AppBAr></AppBAr>
         <BackButton></BackButton>
-        <PricesTable prizes={prizes} />
+        <PricesTable prizes={props.prizes} />
         <TotalPrice />
         <SubmitButton setShouldShowThankYouModal={setShouldShowThankYouModal} />
         {shouldShowThankYouModal && <ThankYouModal setShouldShowThankYouModal={setShouldShowThankYouModal} />}
--- src/TotalPrice.js
+++ src/TotalPrice.js
@@ -1,7 +1,17 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+
 import { TextField } from '@mui/material';
 
-const TotalPrice = () => {
+const TotalPrice = (props) => {
+    const [totalPrice, setTotalPrice] = useState(0);
+    useEffect = () => {
+        let sum = 0;
+        props.prizes.forEach(element => {
+            if (element.isSelected == true)
+                sum += element.price;
+        });
+        setTotalPrice(sum);
+    }, [onload]
     return (
         <TextField id="outlined-basic" label="סך לתשלום" variant="outlined" disabled={true} value="120" />
     )
