e4f92f442b7d8000a6330f0ee0a357f578662ce2
Zipi Werner <96010136+ZipiWerner@users.noreply.github.com>
2022-01-23 16:09:22 +0200

    feat(Prizes) useEffect (#85)

--- src/PrizesPage.js
+++ src/PrizesPage.js
@@ -3,16 +3,15 @@ import AppBar from "./AppBar";
 import ContinueToPayment from "./ContinueToPayment";
 import BonussCard from './NoticeModal';
 import Prizes from "./Prizes";
-import PrizesSum from "./PrizesSum ";
+import PrizesSum from './PrizesSum'
 
 const PrizesPage = () => {
     const [showNoticeModal, setShowNoticeModal] = useState(false);
     return (
-        <>
-            <AppBar>
-                <PrizesSum />
-            </AppBar>
-            <Prizes />
+        <><AppBar>
+            <PrizesSum />
+        </AppBar>
+            <Prizes setShowNoticeModal={setShowNoticeModal} />
             <ContinueToPayment />
             {showNoticeModal && <BonussCard showNoticeModal={setShowNoticeModal} />}
         </>
