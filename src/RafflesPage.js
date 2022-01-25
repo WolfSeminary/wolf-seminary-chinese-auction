094b22d33b486c058d93478f568978b56e2c08ad
efratamar <96010307+efratamar@users.noreply.github.com>
2022-01-25 16:06:13 +0200

    feat(RafflePage): AddRaffleFunctionEfrat (#81)
    * AddRaffleFunctionEfrat
    
    * Delete Prize.js
    
    * Delete PricesTable.js

--- src/RafflesPage.js
+++ src/RafflesPage.js
@@ -1,13 +1,22 @@
-import React from "react";
+import React, { useState } from "react";
 import AppBarPage from "./AppBarPage";
-import Winner from './Winner'
-import RafflesTable from "./RaflesTable ";
-const RafflesPage = (props) => {
+import RaflesTable from RaflesTable
+import { useState } from "react";
+
+const RafflesPage = () => {
+    const [curentRaffle, setCurentRaffle] = useState(null)
+    const [winner, setWinner] = useState(null)
+    const onMakeRaffleClick = () => {
+        const users = localStorage.getItem('users');
+        const prize = localStorage.getItem('prizeName');
+        setCurentRaffle(prize);
+        const rand = Math.floor(Math.random() * users.length);
+        setWinner(users[rand]);
+    }
     return (<>
-        <AppBarPage/>
-        <RafflesTable prizes={props.prizes}/>
-        <Winner firstname={""} lastName={""} currentPrize={""} />
+        <AppBarPage></AppBarPage>
+        <RaflesTable></RaflesTable>
+        <Winner firstname={ } lastName={ } currentPrize={ } ></Winner>
     </>)
 }
-
 export default RafflesPage;
