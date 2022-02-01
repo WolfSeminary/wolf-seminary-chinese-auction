7b3cf1d654be666ff8274bf6ea9bbbf70a646160
eini-rc <REini@madisoncres.com>
2022-02-01 18:44:08 +0200

    style(AppBar): return to prizes in header

--- src/AppBarPage.js
+++ src/AppBarPage.js
@@ -1,22 +1,25 @@
-import React from 'react';
-import AppBar from '@mui/material/AppBar';
-import Box from '@mui/material/Box';
-import Toolbar from '@mui/material/Toolbar';
-import Typography from '@mui/material/Typography';
-import PrizesSum from './PrizesSum'
+import React from "react";
+import AppBar from "@mui/material/AppBar";
+import Box from "@mui/material/Box";
+import Toolbar from "@mui/material/Toolbar";
+import Typography from "@mui/material/Typography";
+import PrizesSum from "./PrizesSum";
 
-export default function AppBarPage(props) {
-    return (
-        <Box sx={{ flexGrow: 1 }}>
-            <AppBar position="static" style={{ textAlign: "center" }}>
-                <Toolbar>
-                    {props.numOfSelectedPrizes >= 0 && <PrizesSum numOfSelectedPrizes={props.numOfSelectedPrizes} />}
-                    <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
-                        ברוכים הבאים למכירת החסד של סמינר וולף
-                        <p style={{ margin: '0px' }}>מפעל הבית</p>
-                    </Typography>
-                </Toolbar>
-            </AppBar>
-        </Box>
-    );
+export default function AppBarPage({ numOfSelectedPrizes, children }) {
+  return (
+    <Box sx={{ flexGrow: 1 }}>
+      <AppBar position="static" style={{ textAlign: "center" }}>
+        <Toolbar>
+          {numOfSelectedPrizes >= 0 && (
+            <PrizesSum numOfSelectedPrizes={numOfSelectedPrizes} />
+          )}
+          {children}
+          <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+            ברוכים הבאים למכירת החסד של סמינר וולף
+            <p style={{ margin: "0px" }}>מפעל הבית</p>
+          </Typography>
+        </Toolbar>
+      </AppBar>
+    </Box>
+  );
 }
